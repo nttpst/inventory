f059fc300c5604f581df83ef37b9de3d45663532
ireshev ivan <nttpst.develop@gmail.com>
2019-03-23 21:03:20 +0300

    прописан первый TextView

--- app/src/main/java/com/vehseri/inventory/MainActivity.java
+++ app/src/main/java/com/vehseri/inventory/MainActivity.java
@@ -2,12 +2,21 @@ package com.vehseri.inventory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    /* method: initialisation of Views */
+    private void initViews(){
+        textHello = findViewById(R.id.text_hello);
     }
 }
