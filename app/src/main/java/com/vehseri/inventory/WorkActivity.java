0c511b729f891bf5661e234ec67f48cfa2883eb7
ireshev ivan <nttpst.develop@gmail.com>
2019-03-26 15:14:28 +0300

    playing with menu

--- app/src/main/java/com/vehseri/inventory/WorkActivity.java
+++ app/src/main/java/com/vehseri/inventory/WorkActivity.java
@@ -3,6 +3,8 @@ package com.vehseri.inventory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class WorkActivity extends AppCompatActivity {
 
@@ -11,9 +13,35 @@ public class WorkActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work);
     }
+    /* menu creating */
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menuItemInventory: {
+                Toast.makeText(this, "Переход на вкладку оборудования", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            case R.id.menuItemAccess: {
+                Toast.makeText(this, "Переход на вкладку доступов", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            case R.id.menuItemAbout: {
+                Toast.makeText(this, "Автор. Версия. Связь", Toast.LENGTH_LONG).show();
+                return true;
+            }
+            case R.id.menuItemExit: {
+                System.exit(0);
+                return true;
+            }
+            default:{
+                return false;
+            }
+        }
     }
 }
