0bc332dd1114dbd79da3ceb7a863b3d6487da043
ireshev ivan <nttpst.develop@gmail.com>
2019-03-27 13:44:59 +0300

    created Inventory Activity

---
+++ app/src/main/java/com/vehseri/inventory/InventoryActivity.java
@@ -0,0 +1,14 @@
+package com.vehseri.inventory;
+
+import android.os.Bundle;
+
+public class InventoryActivity extends WorkActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory);
+    }
+
+
+}
